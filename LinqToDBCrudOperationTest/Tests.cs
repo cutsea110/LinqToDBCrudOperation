4fd4dd53c318eb96ca21a28871cd52126bcf4af0
cutsea110 <cutsea110@gmail.com>
2019-06-17 08:53:26 +0900

    test pass for SQL Server and PostgreSQL without Oracle
    I can't create database on Oracle18c

--- LinqToDBCrudOperationTest/Tests.cs
+++ LinqToDBCrudOperationTest/Tests.cs
@@ -1,12 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using LinqToDB;
+using LinqToDB.Data;
+using LinqToDB.Mapping;
+using NUnit.Framework;
+
+using System.Data.SqlTypes;
 
 namespace LinqToDBCrudOperationTest
 {
+    [TestFixture]
     public class Tests
     {
+        static Tests()
+        {
+            DataConnection.TurnTraceSwitchOn();
+            DataConnection.WriteTraceLine = (msg, context) => Debug.WriteLine(msg, context);
+        }
+
+        [Table]
+        public class TestTable
+        {
+            [PrimaryKey, Identity]
+            public int ID;
+            [Column, NotNull]
+            public string Name;
+            [Column, Nullable]
+            public string Description;
+        }
+
+        [Test]
+        public void CreateTest([ValuesAttribute(ProviderName.SqlServer, ProviderName.PostgreSQL)]string configString)
+        {
+            using(var db = new DataConnection(configString))
+            {
+                try{ db.DropTable<TestTable>(); } catch { }
+                db.CreateTable<TestTable>();
+            }
+        }
     }
 }
